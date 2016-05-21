56cdf57cfcd2b87b866cbfeb30a49455b75db98b
Dennis Matveyev <dennyamd@gmail.com>
2016-05-20 20:18:14 -0400

    update email to me/them

--- module/Commission/src/Mail/Mail.php
+++ module/Commission/src/Mail/Mail.php
@@ -9,7 +9,7 @@ class Mail
 
     function sendMail($id, array $data)
     {
-        $body = "New Commission from {$id}.
+        $body = "Your Commission for your records.
         Request:  {$data['request']}
         ";
         $transport = new Mail\Transport\Sendmail();
@@ -18,12 +18,16 @@ class Mail
         $mail->setBody($body);
         $mail->setFrom('service@furdiversion.net', 'Fur Diversion');
         $mail->addTo($data['email'], $data['name']);
-        $mail->setSubject('New FurDiversion Commission from ' . $data['name'] . '!');
+        $mail->setSubject('Your FurDiversion Commission!');
         $transport->send($mail);
 
         if ('service@furdiversion.net' == $data['email']) {
             return;
         }
+
+        $body = "New Commission from {$id}.
+        Request:  {$data['request']}
+        ";
 
         // mail to me
         $mail = new Mail\Message();
